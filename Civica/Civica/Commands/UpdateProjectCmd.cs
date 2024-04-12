8abdbdb6e22ae7bf2beff2efd74f593abb3acf27
Markusrgg <rggmarkus@gmail.com>
2024-04-12 12:27:28 +0200

    new branch

---
+++ Civica/Civica/Commands/UpdateProjectCmd.cs
@@ -0,0 +1,63 @@
+﻿using Civica.Models;
+using Civica.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Civica.Commands
+{
+    public class UpdateProjectCmd : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        private MainViewModel mvm;
+
+        public UpdateProjectCmd(MainViewModel mvm)
+        {
+            this.mvm = mvm;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            bool succes = false;
+
+            //if (parameter is MainViewModel mvm)
+            //{
+            //    if (!projectRepo.Get(mvm.Selected.Id) is not null) 
+            //    {
+            //        succes = true;
+            //    }
+            //}
+            return succes;
+        }
+
+        public void Execute(object? parameter)
+        {
+            //if (parameter is MainViewModel mvm)
+            //{
+            //    if (projectRepo.GetAll().Find(x => x.Name.ToLower() == mvm.SelectedProject.Name.ToLower()) is not null)
+            //    {
+            //        mvm.RemoveProject(mvm.SelectedProject);
+            //        MessageBox.Show($"Projekt '{mvm.SelectedProject.Name}' slettet!\nTryk for at afslutte.");
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show($"Projekt '{mvm.SelectedProject.Name}' findes ikke!\nTryk for at afslutte.");
+            //    }
+            //}
+        }
+    }
+}
---
+++ Civica/Civica/ViewModels/UpdateProjectViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Civica.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Civica.ViewModels
+{
+    public class UpdateProjectViewModel : INotifyPropertyChanged
+    {
+        private MainViewModel mvm;
+        private ProjectViewModel selectedProject;
+
+        public ProjectViewModel SelectedProject
+        {
+            get { return selectedProject; }
+            set { selectedProject = value; OnPropertyChanged(nameof(SelectedProject)); }
+        }
+        public ICommand UpdateProjectCmd { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public UpdateProjectViewModel(MainViewModel mvm)
+        {
+            this.mvm = mvm;
+            UpdateProjectCmd = new UpdateProjectCmd(mvm);
+        }
+        public void UpdateProject(string name, string owner, string leader, string description)
+        {
+
+        }
+    }
+}
