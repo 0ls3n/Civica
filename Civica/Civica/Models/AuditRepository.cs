74096409e18fa76e54ebca8540091fc957b2b35b
Rasmus <rolfen@gmail.com>
2024-04-17 13:10:14 +0200

    Audit classes added

--- Civica/Civica/Models/AuditRepository.cs
+++ Civica/Civica/Models/AuditRepository.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Civica.Models
-{
-    internal class AuditRepository
-    {
-    }
-}
+//namespace Civica.Models
+//{
+//    public class AuditRepository
+//    {
+//        private List<Audit> _auditList = new List<Audit>();
+
+//        public AuditRepository()
+//        {
+            
+//        }
+//        public List<Audit> GetAll() => _auditList;
+
+//        public void Add(Audit audit)
+//        {
+//            _auditList.Add(audit);
+//        }
+
+//        public void Remove(Audit audit)
+//        {
+//            _auditList.Remove(audit);
+//        }
+//        public Audit Get(int id)
+//        {
+//            return _auditList.Find(x => x.Id == id);
+//        }
+
+//    }
+//}
