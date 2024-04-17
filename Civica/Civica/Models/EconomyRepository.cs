87a7e2778b11ec9113f66d289d323d220f2217f6
Rasmus <rolfen@gmail.com>
2024-04-17 12:11:55 +0200

    auditfeatures added

---
+++ Civica/Civica/Models/EconomyRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Civica.Models
+{
+    public class EconomyRepository
+    {
+        private List<Economy> _economyList = new List<Economy>();
+        public EconomyRepository() { }
+
+        public List<Economy> GetAll()=> _economyList;
+        
+        public void Add(Economy economy)
+        {
+            _economyList.Add(economy);
+        }
+        
+        public void Remove(Economy economy) 
+        {
+            _economyList.Remove(economy);
+        }
+        public Economy Get(int id)
+        {
+            return _economyList.Find(x => x.Id == id);
+        }
+    }
+}
