a2b581024d0c23ae4df0df758f6143ee9763664d
Rasmus Olsen <rasmus782@gmail.com>
2024-04-19 11:40:22 +0200

    Created the basic UI For the new user interface

--- Civica/Civica/ViewModels/MainViewModel.cs
+++ Civica/Civica/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -13,107 +14,111 @@ using Civica.Models.Enums;
 
 namespace Civica.ViewModels
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : ObservableObject
     {
-        public ObservableCollection<ProjectViewModel> Projects { get; set; } = new ObservableCollection<ProjectViewModel>();
-        public ObservableCollection<ProgressViewModel> SelectedProgresses { get; set; } = new ObservableCollection<ProgressViewModel>();
+        #region Old Code
+        //public ObservableCollection<ProjectViewModel> Projects { get; set; } = new ObservableCollection<ProjectViewModel>();
+        //public ObservableCollection<ProgressViewModel> SelectedProgresses { get; set; } = new ObservableCollection<ProgressViewModel>();
 
-        private ProjectRepository projectRepo = new ProjectRepository();
-        private ProgressRepository progressRepo = new ProgressRepository();
-        private ProjectViewModel selectedProject = null;
-        public ProjectViewModel SelectedProject
-        {
-            get
-            {
-                return selectedProject;
-            }
-            set
-            {
-                selectedProject = value;
-                OnPropertyChanged(nameof(SelectedProject));
-                OnPropertyChanged(nameof(CanUpdateProject));
+        //private ProjectRepository projectRepo = new ProjectRepository();
+        //private ProgressRepository progressRepo = new ProgressRepository();
+        //private ProjectViewModel selectedProject = null;
+        //public ProjectViewModel SelectedProject
+        //{
+        //    get
+        //    {
+        //        return selectedProject;
+        //    }
+        //    set
+        //    {
+        //        selectedProject = value;
+        //        OnPropertyChanged(nameof(SelectedProject));
+        //        OnPropertyChanged(nameof(CanUpdateProject));
 
-                SelectedProgresses.Clear();
+        //        SelectedProgresses.Clear();
 
-                foreach (Progress prog in progressRepo.Get(SelectedProject.GetId()))
-                {
-                    SelectedProgresses.Add(new ProgressViewModel(prog));
-                }
-            }
-        }
-        private ProgressViewModel selectedProgress = null;
-        public ProgressViewModel SelectedProgress
-        {
-            get
-            {
-                return selectedProgress;
-            }
-            set
-            {
-                selectedProgress = value;
-                OnPropertyChanged(nameof(SelectedProgress));
-            }
-        }
+        //        foreach (Progress prog in progressRepo.Get(SelectedProject.GetId()))
+        //        {
+        //            SelectedProgresses.Add(new ProgressViewModel(prog));
+        //        }
+        //    }
+        //}
+        //private ProgressViewModel selectedProgress = null;
+        //public ProgressViewModel SelectedProgress
+        //{
+        //    get
+        //    {
+        //        return selectedProgress;
+        //    }
+        //    set
+        //    {
+        //        selectedProgress = value;
+        //        OnPropertyChanged(nameof(SelectedProgress));
+        //    }
+        //}
 
-        public bool CanUpdateProject => SelectedProject != null;
+        //public bool CanUpdateProject => SelectedProject != null;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        //public MainViewModel()
+        //{
+        //    foreach (Project p in projectRepo.GetAll())
+        //    {
+        //        Projects.Add(new ProjectViewModel(p));
+        //    }
+        //}
 
-        private void OnPropertyChanged(string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        //public void CreateNewProject(string name, string owner = "", string manager = "", string description = "")
+        //{
+        //    Project p = new Project(name, owner, manager, description);
 
-        public MainViewModel()
-        {
-            foreach (Project p in projectRepo.GetAll())
-            {
-                Projects.Add(new ProjectViewModel(p));
-            }
-        }
+        //    projectRepo.Add(p);
 
-        public void CreateNewProject(string name, string owner = "", string manager = "", string description = "")
-        {
-            Project p = new Project(name, owner, manager, description);
+        //    ProjectViewModel pvm = new ProjectViewModel(p);
 
-            projectRepo.Add(p);
+        //    Projects.Add(pvm);
+        //}
 
-            ProjectViewModel pvm = new ProjectViewModel(p);
+        //public void UpdateProject(ProjectViewModel project, string name, string owner = "", string manager = "", string description = "")
+        //{
+        //    int index = Projects.IndexOf(project);
 
-            Projects.Add(pvm);
-        }
+        //    Projects[index] = null;
 
-        public void UpdateProject(ProjectViewModel project, string name, string owner = "", string manager = "", string description = "")
-        {
-            int index = Projects.IndexOf(project);
+        //    project.Name = name;
+        //    project.Owner = owner;
+        //    project.Manager = manager;
+        //    project.Description = description;
 
-            Projects[index] = null;
+        //    Projects[index] = project;
 
-            project.Name = name;
-            project.Owner = owner;
-            project.Manager = manager;
-            project.Description = description;
+        //    projectRepo.Update(projectRepo.Get(project.GetId()), name, owner, manager, description);
+        //}
 
-            Projects[index] = project;
+        //public void RemoveProject()
+        //{
+        //    Project p = projectRepo.Get(SelectedProject.GetId());
+        //    projectRepo.Remove(p);
+        //    Projects.Remove(SelectedProject);
+        //}
 
-            projectRepo.Update(projectRepo.Get(project.GetId()), name, owner, manager, description);
-        }
+        //public void ProgressProject(Phase fase, Status status, string description)
+        //{
+        //    Progress prog = new Progress(SelectedProject.GetId(), fase, status, DateTime.Now, description);
+        //    prog.ProjectId = SelectedProject.GetId();
 
-        public void RemoveProject()
-        {
-            Project p = projectRepo.Get(SelectedProject.GetId());
-            projectRepo.Remove(p);
-            Projects.Remove(SelectedProject);
-        }
+        //    progressRepo.Add(prog);
 
-        public void ProgressProject(Phase fase, Status status, string description)
-        {
-            Progress prog = new Progress(SelectedProject.GetId(), fase, status, DateTime.Now, description);
-            prog.ProjectId = SelectedProject.GetId();
+        //    SelectedProgresses.Add(new ProgressViewModel(prog));
+        //}
 
-            progressRepo.Add(prog);
+        #endregion
 
-            SelectedProgresses.Add(new ProgressViewModel(prog));
-        }
+        #region New code
+
+
+
+
+
+        #endregion
     }
 }
