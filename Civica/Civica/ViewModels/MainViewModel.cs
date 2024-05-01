0c0a3c1f121e4a16d963888ed0519d4ccbbbcb0a
Rasmus Olsen <rasmus782@gmail.com>
2024-05-01 13:25:19 +0200

    Deleted some old code

--- Civica/Civica/ViewModels/MainViewModel.cs
+++ Civica/Civica/ViewModels/MainViewModel.cs
@@ -18,104 +18,6 @@ namespace Civica.ViewModels
 {
     public class MainViewModel : ObservableObject
     {
-        #region Old Code
-        //public ObservableCollection<ProjectViewModel> Projects { get; set; } = new ObservableCollection<ProjectViewModel>();
-        //public ObservableCollection<ProgressViewModel> SelectedProgresses { get; set; } = new ObservableCollection<ProgressViewModel>();
-
-        //private ProjectRepository projectRepo = new ProjectRepository();
-        //private ProgressRepository progressRepo = new ProgressRepository();
-        //private ProjectViewModel selectedProject = null;
-        //public ProjectViewModel SelectedProject
-        //{
-        //    get
-        //    {
-        //        return selectedProject;
-        //    }
-        //    set
-        //    {
-        //        selectedProject = value;
-        //        OnPropertyChanged(nameof(SelectedProject));
-        //        OnPropertyChanged(nameof(CanUpdateProject));
-
-        //        SelectedProgresses.Clear();
-
-        //        foreach (Progress prog in progressRepo.Get(SelectedProject.GetId()))
-        //        {
-        //            SelectedProgresses.Add(new ProgressViewModel(prog));
-        //        }
-        //    }
-        //}
-        //private ProgressViewModel selectedProgress = null;
-        //public ProgressViewModel SelectedProgress
-        //{
-        //    get
-        //    {
-        //        return selectedProgress;
-        //    }
-        //    set
-        //    {
-        //        selectedProgress = value;
-        //        OnPropertyChanged(nameof(SelectedProgress));
-        //    }
-        //}
-
-        //public bool CanUpdateProject => SelectedProject != null;
-
-        //public MainViewModel()
-        //{
-        //    foreach (Project p in projectRepo.GetAll())
-        //    {
-        //        Projects.Add(new ProjectViewModel(p));
-        //    }
-        //}
-
-        //public void CreateNewProject(string name, string owner = "", string manager = "", string description = "")
-        //{
-        //    Project p = new Project(name, owner, manager, description);
-
-        //    projectRepo.Add(p);
-
-        //    ProjectViewModel pvm = new ProjectViewModel(p);
-
-        //    Projects.Add(pvm);
-        //}
-
-        //public void UpdateProject(ProjectViewModel project, string name, string owner = "", string manager = "", string description = "")
-        //{
-        //    int index = Projects.IndexOf(project);
-
-        //    Projects[index] = null;
-
-        //    project.Name = name;
-        //    project.Owner = owner;
-        //    project.Manager = manager;
-        //    project.Description = description;
-
-        //    Projects[index] = project;
-
-        //    projectRepo.Update(projectRepo.Get(project.GetId()), name, owner, manager, description);
-        //}
-
-        //public void RemoveProject()
-        //{
-        //    Project p = projectRepo.Get(SelectedProject.GetId());
-        //    projectRepo.Remove(p);
-        //    Projects.Remove(SelectedProject);
-        //}
-
-        //public void ProgressProject(Phase fase, Status status, string description)
-        //{
-        //    Progress prog = new Progress(SelectedProject.GetId(), fase, status, DateTime.Now, description);
-        //    prog.ProjectId = SelectedProject.GetId();
-
-        //    progressRepo.Add(prog);
-
-        //    SelectedProgresses.Add(new ProgressViewModel(prog));
-        //}
-
-        #endregion
-
-        #region New code
 
         private string _viewTitle;
         public string ViewTitle
@@ -231,6 +133,5 @@ namespace Civica.ViewModels
 
         public IRepository<Project> GetProjectRepo() => projectRepo;
         public IRepository<Progress> GetProgressRepo() => progressRepo;
-        #endregion
     }
 }
