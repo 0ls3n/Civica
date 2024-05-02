fc93c9a9eb3fbc4b6d5a82fd554839414eb16e0a
Rasmus Olsen <rasmus782@gmail.com>
2024-05-02 12:19:25 +0200

    Deleted the unittest

--- Civica/UnitTest/UnitTest1.cs
+++ Civica/UnitTest/UnitTest1.cs
@@ -5,24 +5,6 @@ namespace UnitTest
     [TestClass]
     public class UnitTest1
     {
-        //Project p = new Project("test", "123", "321", "Blah Blah Blah"); // ARRANGE
-        //ProjectRepository repo = new ProjectRepository();
-
-
-        //[TestMethod]
-        //public void TestAddProjectToDatabase()
-        //{
-        //    repo.Add(p); // ACT
-
-        //    Assert.IsNotNull(repo.Get(p.Id)); // ASSERT
-        //}
-
-        //[TestMethod]
-        //public void TestRemoveProjectFromDatabase()
-        //{
-        //    repo.Remove(repo.Get(p.Id)); // ACT
-
-        //    Assert.IsNull(repo.Get(p.Id)); // ASSERT
-        //}
+        
     }
 }
