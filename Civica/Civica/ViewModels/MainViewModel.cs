ff9b332c4e3f5e3cf9e42d1c2fdebd309b86b22f
Rasmus Olsen <rasmus782@gmail.com>
2024-05-06 11:08:33 +0200

    Deleted a line of code

--- Civica/Civica/ViewModels/MainViewModel.cs
+++ Civica/Civica/ViewModels/MainViewModel.cs
@@ -234,7 +234,6 @@ namespace Civica.ViewModels
         }
 
         System.Timers.Timer db_timer;
-        System.Timers.Timer ul_timer;
 
         private void StartTimerDatabaseRefresh()
         {
@@ -252,6 +251,12 @@ namespace Civica.ViewModels
             await userRepo.RefreshAsync();
 
             await Task.Delay(1000);
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                ipvm.UpdateList();
+                svm.UpdateList();
+            });
         }
 
         public IRepository<Project> GetProjectRepo() => projectRepo;
