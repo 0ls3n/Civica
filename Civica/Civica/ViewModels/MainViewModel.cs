2ee2f913714fdfa1d6108237f2bb8a1852c098db
Rasmus Olsen <rasmus782@gmail.com>
2024-05-06 10:56:04 +0200

    Made a async function for reading from the database

--- Civica/Civica/ViewModels/MainViewModel.cs
+++ Civica/Civica/ViewModels/MainViewModel.cs
@@ -9,10 +9,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Timers;
 using Civica.Commands;
 using Civica.Interfaces;
 using Civica.Models;
 using Civica.Models.Enums;
+using System.Windows.Threading;
 
 namespace Civica.ViewModels
 {
@@ -92,6 +94,7 @@ namespace Civica.ViewModels
                     mvm.ExpandedProjectView = WindowVisibility.Hidden;
                     mvm.SettingsView = WindowVisibility.Hidden;
                     mvm.ViewTitle = mvm.ipvm.WindowTitle;
+                    mvm.ipvm.UpdateList();
                 }
             },
             canExecute: (object? parameter) =>
@@ -181,6 +184,29 @@ namespace Civica.ViewModels
             SettingsView = WindowVisibility.Hidden;
             
             ViewTitle = ipvm.WindowTitle;
+
+            StartTimerDatabaseRefresh();
+        }
+
+        System.Timers.Timer db_timer;
+        System.Timers.Timer ul_timer;
+
+        private void StartTimerDatabaseRefresh()
+        {
+            db_timer = new System.Timers.Timer(2500);
+            db_timer.Elapsed += async (sender, e) => await RefreshDataAsync();
+            db_timer.AutoReset = true;
+            db_timer.Start();
+        }
+
+        private async Task RefreshDataAsync()
+        {
+            await projectRepo.RefreshAsync();
+            await progressRepo.RefreshAsync();
+            await resourceRepo.RefreshAsync();
+            await userRepo.RefreshAsync();
+
+            await Task.Delay(1000);
         }
 
         public IRepository<Project> GetProjectRepo() => projectRepo;
