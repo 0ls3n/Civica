d3ba19350027228a54931ebd5b03e730b7b7c193
Rasmus Olsen <rasmus782@gmail.com>
2024-05-06 10:56:04 +0200

    Made a async function for reading from the database

--- Civica/Civica/ViewModels/MainViewModel.cs
+++ Civica/Civica/ViewModels/MainViewModel.cs
@@ -8,10 +8,12 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
+using System.Timers;
 using Civica.Commands;
 using Civica.Interfaces;
 using Civica.Models;
 using Civica.Models.Enums;
+using System.Windows.Threading;
 
 namespace Civica.ViewModels
 {
@@ -244,6 +246,27 @@ namespace Civica.ViewModels
             ViewTitle = ipvm.WindowTitle;
 
             StartTimerDatabaseRefresh();
+        }
+
+        System.Timers.Timer db_timer;
+        System.Timers.Timer ul_timer;
+
+        private void StartTimerDatabaseRefresh()
+        {
+            db_timer = new System.Timers.Timer(2500);
+            db_timer.Elapsed += async (sender, e) => await RefreshDataAsync();
+            db_timer.AutoReset = true;
+            db_timer.Start();
+        }
+
+        private async Task RefreshDataAsync()
+        {
+            await projectRepo.RefreshAsync();
+            await progressRepo.RefreshAsync();
+            await resourceRepo.RefreshAsync();
+            await userRepo.RefreshAsync();
+
+            await Task.Delay(1000);
         }
 
         public IRepository<Project> GetProjectRepo() => projectRepo;
