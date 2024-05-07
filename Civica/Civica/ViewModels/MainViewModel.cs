87ff7d237206e5b8380de91e9d4692d43f8063a6
Rasmus Olsen <rasmus782@gmail.com>
2024-05-07 10:31:58 +0200

    Added the new repo to async method

--- Civica/Civica/ViewModels/MainViewModel.cs
+++ Civica/Civica/ViewModels/MainViewModel.cs
@@ -254,6 +254,7 @@ namespace Civica.ViewModels
             await progressRepo.RefreshAsync();
             await resourceRepo.RefreshAsync();
             await userRepo.RefreshAsync();
+            await auditRepo.RefreshAsync();
 
             Application.Current.Dispatcher.Invoke(() =>
             {
