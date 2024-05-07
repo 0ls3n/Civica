7c25e42cdffdf5a98598ba75cad62a94d042a073
Rasmus Olsen <rasmus782@gmail.com>
2024-05-07 10:28:54 +0200

    Merge conflict resolved

--- Civica/Civica/ViewModels/MainViewModel.cs
+++ Civica/Civica/ViewModels/MainViewModel.cs
@@ -251,6 +251,7 @@ namespace Civica.ViewModels
         public IRepository<Progress> GetProgressRepo() => progressRepo;
         public IRepository<Resource> GetResourceRepo() => resourceRepo;
         public IRepository<User> GetUserRepo() => userRepo;
+        public IRepository<Audit> GetAuditRepo() => auditRepo;
 
         #region AsyncRefresh
         System.Timers.Timer db_timer;
@@ -276,11 +277,6 @@ namespace Civica.ViewModels
                 svm.UpdateList();
             });
         }
-
-        public IRepository<Project> GetProjectRepo() => projectRepo;
-        public IRepository<Progress> GetProgressRepo() => progressRepo;
-        public IRepository<Resource> GetResourceRepo() => resourceRepo;
-        public IRepository<Audit> GetAuditRepo() => auditRepo;
-        public IRepository<User> GetUserRepo() => userRepo;
+        #endregion
     }
 }
